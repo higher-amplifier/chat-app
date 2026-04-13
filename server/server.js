7240d8d36cbc35e57cefe42add5030b6bad03bc5
higher-amplifier <4bharatmeghwal@gmail.com>
2026-04-13 23:15:48 +0530

    fix body size limit for image upload

--- server/server.js
+++ server/server.js
@@ -43,8 +43,9 @@ io.on("connection", socket => {
   });
 });
 
-app.use(express.json());
-// middleware
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 app.use(cors({
   origin: [
     "http://localhost:5173",
